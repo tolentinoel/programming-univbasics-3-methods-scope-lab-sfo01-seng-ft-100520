b003407a65673d17a81f639cc4c37dca23864fb9
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-13 19:03:24 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,7 +4,7 @@ def mario
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
-status = 'Thank You Mario! But Our Princess Is In Another Castle!'
