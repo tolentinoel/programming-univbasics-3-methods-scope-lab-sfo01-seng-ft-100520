0d79ccea05bc33e8fdb4def1d4eb966abb312f24
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-13 19:08:34 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -14,7 +14,7 @@ def link
 end
 
 def all_phrases
-  puts #{mario}
-  puts #{toadstool}
-  puts #{link}
+  puts "It's-a me, Mario!"
+  puts 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts "It's Dangerous To Go Alone! Take This."
 end
